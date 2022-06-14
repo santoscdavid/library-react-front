3202df4c3219df7584516d660770ae871c60d89f
DSantos69 <david.csantos69@gmail.com>
2022-06-14 09:17:30 -0300

    #15 added pizza chart

--- src/views/Dashboard/components/DashboardGrid.jsx
+++ src/views/Dashboard/components/DashboardGrid.jsx
@@ -1,16 +1,23 @@
 import { Box } from '@mui/material';
 import BooksTotal from './BooksTotal';
 import CustomersTotal from './CustomersTotal';
+import PizzaChart from './PizzaChart';
 import PublishersTotal from './PublishersTotal';
 import RentsTotal from './RentsTotal';
 
 export default function DashboardGrid() {
     return (
-        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', mt: 7 }}>
-            <PublishersTotal />
-            <BooksTotal />
-            <CustomersTotal />
-            <RentsTotal />
-        </Box>
+        <>
+            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', mt: 7 }}>
+                <PublishersTotal />
+                <BooksTotal />
+                <CustomersTotal />
+                <RentsTotal />
+            </Box>
+            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', mt: 7 }}>
+                <PizzaChart />
+                <PizzaChart />
+            </Box>
+        </>
     );
 }
---
+++ src/views/Dashboard/components/PizzaChart.jsx
@@ -0,0 +1,74 @@
+import { Card, CardContent, CardHeader } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import api from '../../../configs/api';
+
+export default function PizzaChart() {
+    const [rents, setRents] = useState([]);
+
+    const getRents = () => {
+        api.get('/aluguel').then(({ data }) => {
+            setRents(data);
+        });
+    };
+
+    function filterOnDeadline(obj) {
+        if ('devolucao' in obj && obj.devolucao <= obj.previsaoEntrega) {
+            return true;
+        }
+    }
+
+    function filterNotReturned(obj) {
+        if ('devolucao' in obj && obj.devolucao === null) {
+            return true;
+        }
+    }
+
+    function filterOnDelay(obj) {
+        if ('devolucao' in obj && obj.devolucao > obj.previsaoEntrega) {
+            return true;
+        }
+    }
+
+    var onTime = rents.filter(filterOnDeadline);
+    var notReturned = rents.filter(filterNotReturned);
+    var onDelay = rents.filter(filterOnDelay);
+
+    useEffect(() => {
+        getRents();
+    }, []);
+
+    const donutChart = {
+        series: [notReturned.length, onTime.length, onDelay.length],
+        chart: {
+            type: 'pie'
+        },
+        labels: ['Não devolvido', 'No prazo', 'Em atraso'],
+        colors: ['#5AB55E', '#F55246', '#FEB019'],
+        fill: {
+            colors: ['#5AB55E', '#F55246', '#FEB019']
+        },
+        responsive: [
+            {
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: 200
+                    },
+                    legend: {
+                        position: 'bottom'
+                    }
+                }
+            }
+        ]
+    };
+
+    return (
+        <Card sx={{ minWidth: 275, m: 1, p: 1 }}>
+            <CardHeader title="Média dos status de Aluguel" />
+            <CardContent>
+                <Chart options={donutChart} series={donutChart.series} type="donut" width={450} height={350} />
+            </CardContent>
+        </Card>
+    );
+}
